30957fcae7784880558504acc73e49fe9ef0d7a7
s0ham075 <sohamsawant075@gmail.com>
2024-12-27 12:40:40 +0530

    Restrict grid access

--- chain_reaction/chain_reaction.py
+++ chain_reaction/chain_reaction.py
@@ -1,7 +1,6 @@
-# chain reaction game for 2 players
 
 class Dot():
-    def __init__(self,y,x,player_no, atoms_no):
+    def init(self,y,x,player_no, atoms_no):
         self.x = x
         self.y = y
         self.atoms = atoms_no
@@ -13,7 +12,7 @@ class Dot():
         if (self.x >= 1 and self.y < (length -1)): # bottom left
             self.type += 1
             self.neighbour_coords.append([self.y+1,self.x-1])
-        
+
         if (self.x < (width -1) and self.y < (length -1)): # bottom right
             self.type += 1
             self.neighbour_coords.append([self.y+1,self.x+1])
@@ -31,7 +30,7 @@ class ChainReaction():
     def __init__(self, length, width):
         self.grid_length = length
         self.grid_width = width 
-        self.grid = []
+        self.__grid = []  # Made private with double underscore
 
         # initialize grid
         for j in range(length):
@@ -41,33 +40,37 @@ class ChainReaction():
                 dot.set_type(length,width)
                 row.append(dot)
 
-            self.grid.append(row)
+            self.__grid.append(row)
+
+    def get_grid(self):
+        """Returns the current game state grid"""
+        return self.__grid
 
     def add_dot(self,y,x, player_no):
         if (x >= self.grid_width or y >= self.grid_length): # should not be out of grid
             return
         
-        if (self.grid[y][x].owner != 0 and self.grid[y][x].owner != player_no): # should not be on other players territory
+        if (self.__grid[y][x].owner != 0 and self.__grid[y][x].owner != player_no): # should not be on other players territory
             return
         
-        self.grid[y][x].atoms += 1
-        self.grid[y][x].owner = player_no
+        self.__grid[y][x].atoms += 1
+        self.__grid[y][x].owner = player_no
 
-        if(self.grid[y][x].type == self.grid[y][x].atoms):
+        if(self.__grid[y][x].type == self.__grid[y][x].atoms):
             self.explode(y,x,player_no)
         
     def explode(self,y,x, player_no):
-        self.grid[y][x].atoms = 0
-        self.grid[y][x].owner = 0
-        for y1,x1 in self.grid[y][x].neighbour_coords:
-            self.grid[y1][x1].owner = player_no
-            self.grid[y1][x1].atoms += 1
-            if(self.grid[y1][x1].type == self.grid[y1][x1].atoms):
+        self.__grid[y][x].atoms = 0
+        self.__grid[y][x].owner = 0
+        for y1,x1 in self.__grid[y][x].neighbour_coords:
+            self.__grid[y1][x1].owner = player_no
+            self.__grid[y1][x1].atoms += 1
+            if(self.__grid[y1][x1].type == self.__grid[y1][x1].atoms):
                 self.explode(y1,x1,player_no)
 
     def check_game_over(self):
         owner = -1
-        for row in self.grid:
+        for row in self.__grid:
             for dot in row:
                 if dot.owner != 0:  # Ignore unoccupied cells
                     if owner == -1:
@@ -75,8 +78,4 @@ class ChainReaction():
                     elif owner != dot.owner:
                         return -1  # More than one player owns cells
                     
-        return owner 
-
-        
-        
-       
+        return owner
